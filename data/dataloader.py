912e94a2f251462f6df8857928a097de04cbf116
Emanuele Pietro Cometti s346291 <s346291@studenti.polito.it>
2025-11-04 18:00:25 +0100

    v1.0.2

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,10 +1,18 @@
-from dataset import get_tiny_imagenet_datasets
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
 import torch
 
 def get_tiny_imagenet_loaders(root="dataset/tiny-imagenet-200", batch_size=32):
-    train_dataset, val_dataset = get_tiny_imagenet_datasets(root, batch_size)
+    transform = T.Compose([
+        T.Resize((224, 224)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    train_dataset = ImageFolder(root=f"{root}/train", transform=transform)
+    val_dataset = ImageFolder(root=f"{root}/val", transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader
